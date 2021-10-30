f1641e3484913ded1ef4f54ea51598322b73f5b9
William G Ledbetter <williamgledbetter@gmail.com>
2021-10-30 14:48:55 -0600

    enforce 2-thread build to try and minimize ram use

--- setup.py
+++ setup.py
@@ -101,7 +101,14 @@ class CMakeBuild(build_ext):
 
         # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
         # across all generators.
-        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
+        if "GITHUB_WORKSPACE" in os.environ:
+            print("###################################################################")
+            print("###################################################################")
+            print("############ ITS OKAY! YOU ARE ONLY RUNNING TWO CORES! ############")
+            print("###################################################################")
+            print("###################################################################")
+            build_args += ["-j2"]
+        elif "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
             # self.parallel is a Python 3 only way to set parallel jobs by hand
             # using -j in the build_ext call, not supported by pip or PyPA-build.
             if hasattr(self, "parallel") and self.parallel:

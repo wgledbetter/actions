e03ef4d96ec164935f3c4a65e87dc4db30d3d759
William G Ledbetter <williamgledbetter@gmail.com>
2021-10-30 15:08:47 -0600

    look around in macos

--- .github/install_macos_dependencies.sh
+++ .github/install_macos_dependencies.sh
@@ -3,6 +3,9 @@ conda install -c intel mkl-static mkl-devel
 PYTHONEXEC=`which python`
 echo "MKLROOT=${PYTHONEXEC%/bin/python}" >> $GITHUB_ENV
 
-echo ${PYTHONEXEC%/bin/python}
+echo ${PYTHONEXEC%/bin/python}::
+ls ${PYTHONEXEC%/bin/python}
+echo "*********************"
+ls ${PYTHONEXEC%/bin/python}/lib
 echo "*********************"
 find ${PYTHONEXEC%/bin/python} -name "libmkl*"

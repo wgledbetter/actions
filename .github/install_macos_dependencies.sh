7d3a8baa4612458ba88388652a42267a129d2a46
William G Ledbetter <williamgledbetter@gmail.com>
2021-10-30 15:12:45 -0600

    try upping permissions

--- .github/install_macos_dependencies.sh
+++ .github/install_macos_dependencies.sh
@@ -1,7 +1,11 @@
 # Install MKL
-conda install -c intel mkl-static mkl-devel
+sudo conda install -c intel mkl-static mkl-devel
 PYTHONEXEC=`which python`
-echo "MKLROOT=${PYTHONEXEC%/bin/python}" >> $GITHUB_ENV
+sudo echo "MKLROOT=${PYTHONEXEC%/bin/python}" >> $GITHUB_ENV
+
+echo "*********************"
+echo "*********************"
+echo "*********************"
 
 echo ${PYTHONEXEC%/bin/python}::
 ls ${PYTHONEXEC%/bin/python}
